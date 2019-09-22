ded20ae5e4519b7d453b9c55110fe6d0682d23ab
Junpeng He <hejunpeng2012@hotmail.com>
2019-09-22 16:50:46 +0800

    understanding jfx

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 
 class App extends Component{
   render() {
-    return (
-        <div className="App">
-          <h1>Hi, I'm a react app</h1>
-        </div>
-    );
+    // return (
+    //     <div className="App">
+    //       <h1>Hi, I'm a react app</h1>
+    //     </div>
+    // );
+      return React.createElement('div', {className: 'App'},
+          React.createElement('h1', null, 'Does it work now?'), "Hi, I'm a react app!!!")
   }
 }
 
