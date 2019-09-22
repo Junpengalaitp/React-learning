bc031856defdd1d6425ced5e65bea4dc3565913e
Junpeng He <hejunpeng2012@hotmail.com>
2019-09-23 07:45:04 +0800

    better structure for conditional rendering

--- src/App.js
+++ src/App.js
@@ -48,6 +48,26 @@ class App extends Component {
       cursor: 'pointer'
     }
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+      <div>
+        <Person 
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}/>
+        <Person 
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.swithNameHandler.bind(this, 'Junpeng Again')}
+          changed={this.nameChangedHandler}>My hobbies: racing</Person>
+        <Person 
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}/>
+      </div>
+      ) 
+    }
+
     return (
         <div className="App">
           <h1>Hi, I'm a react app</h1>
@@ -55,21 +75,7 @@ class App extends Component {
           <button 
             style={style}
             onClick={this.togglePersonHandler}>Switch Name</button>
-          { this.state.showPersons ? 
-            (<div>
-              <Person 
-                name={this.state.persons[0].name}
-                age={this.state.persons[0].age}/>
-              <Person 
-                name={this.state.persons[1].name}
-                age={this.state.persons[1].age}
-                click={this.swithNameHandler.bind(this, 'Junpeng Again')}
-                changed={this.nameChangedHandler}>My hobbies: racing</Person>
-              <Person 
-                name={this.state.persons[2].name}
-                age={this.state.persons[2].age}/>
-            </div>) : null
-        }
+            {this.state.showPersons ? persons : null}
         </div>
     );
     //   return React.createElement('div', {className: 'App'},
