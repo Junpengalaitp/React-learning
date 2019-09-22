a4944e525428813e90bf07da4a55a7ffda447b34
Junpeng He <hejunpeng2012@hotmail.com>
2019-09-22 17:39:32 +0800

    output dynamic content

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from "./Person/Person";
 
 class App extends Component{
   render() {
-    // return (
-    //     <div className="App">
-    //       <h1>Hi, I'm a react app</h1>
-    //     </div>
-    // );
-      return React.createElement('div', {className: 'App'},
-          React.createElement('h1', null, 'Does it work now?'), "Hi, I'm a react app!!!")
+    return (
+        <div className="App">
+          <h1>Hi, I'm a react app</h1>
+          <p>This is really working</p>
+            <Person />
+            <Person />
+            <Person />
+        </div>
+    );
+    //   return React.createElement('div', {className: 'App'},
+    //       React.createElement('h1', null, 'Does it work now?'), "Hi, I'm a react app!!!")
   }
 }
 
