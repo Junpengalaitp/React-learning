ba8721ae8b649239afaf6ac7acfcac71e3b494db
Junpeng He <hejunpeng2012@hotmail.com>
2019-09-22 20:19:01 +0800

    useState() hook

--- src/App.js
+++ src/App.js
@@ -1,41 +1,58 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from "./Person/Person";
 
-class App extends Component {
-  state = {
-    persons: [
-      {name: 'Max', age: 22},
-      {name: 'Jack', age: 12},
-      {name: 'Ace', age: 33}
-    ],
-    otherState: 'some other value'
-  }
+const App = props => {
+    const [ personsState, setPersonState ] = useState({
+      persons: [
+        {name: 'Max', age: 22},
+        {name: 'Jack', age: 12},
+        {name: 'Ace', age: 33}
+      ],
+      otherState: 'some other value'
+    });
+    
+    const swithNameHandler = (params) => {
+      console.log('Was clicked!');
+      setPersonState({persons: [
+        {name: 'Junpeng Only', age: 27},
+        {name: 'Junpeng Only', age: 27},
+        {name: 'Junpeng Only', age: 27}
+      ],
+      otherState: personsState.otherState
+    })
+    }
 
-  swithNameHandler = (params) => {
-    console.log('Was clicked!');
-    this.setState({persons: [
-      {name: 'Junpeng Only', age: 27},
-      {name: 'Junpeng Only', age: 27},
-      {name: 'Junpeng Only', age: 27}
-    ]})
-  }
-  
-
-  render() {
     return (
         <div className="App">
           <h1>Hi, I'm a react app</h1>
           <p>This is really working</p>
-          <button onClick={this.swithNameHandler}>Switch Name</button>
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: racing</Person>
-          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+          <button onClick={swithNameHandler}>Switch Name</button>
+          <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+          <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My hobbies: racing</Person>
+          <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
         </div>
     );
     //   return React.createElement('div', {className: 'App'},
     //       React.createElement('h1', null, 'Does it work now?'), "Hi, I'm a react app!!!")
-  }
 }
 
 export default App;
+
+// state = {
+//   persons: [
+//     {name: 'Max', age: 22},
+//     {name: 'Jack', age: 12},
+//     {name: 'Ace', age: 33}
+//   ],
+//   otherState: 'some other value'
+// }
+
+// swithNameHandler = (params) => {
+//   console.log('Was clicked!');
+//   this.setState({persons: [
+//     {name: 'Junpeng Only', age: 27},
+//     {name: 'Junpeng Only', age: 27},
+//     {name: 'Junpeng Only', age: 27}
+//   ]})
+// }
