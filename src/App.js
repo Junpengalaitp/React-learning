42bf66cfe16682f9072d99f6f1b37d46177b8604
Junpeng He <hejunpeng2012@hotmail.com>
2019-09-23 20:32:48 +0800

    flexible lists

--- src/App.js
+++ src/App.js
@@ -13,14 +13,19 @@ class App extends Component {
     showPersons: false
   }
 
-  nameChangedHandler = (event) => {
-    this.setState( {persons:
-      [
-        {name: 'Only Junpeng', age: 27},
-        {name: event.target.value, age: 21},
-        {name: 'Ace', age: 33}
-      ]
-    } )
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {...this.state.persons[personIndex]}
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+    
+    this.setState({persons: persons})
   }
 
   deletePersonHandler = (personIndex) => {
@@ -53,7 +58,8 @@ class App extends Component {
                     name={person.name}
                     age={person.age}
                     click={this.deletePersonHandler.bind(this, index)}
-                    key={person.name} />
+                    key={person.id}
+                    changed={(event) => this.nameChangedHandler(event, person.id)} />
           })}
         </div>
       ) 
